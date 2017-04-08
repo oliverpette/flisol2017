0b26610e122ef1c763d89977905f23760ed436cd
Pedro Otavio de Oliveira <otavio.pedro@outlook.com>
2017-04-08 16:38:49 -0300

    incluindo teste1.c
    Signed-off-by: Pedro Otavio de Oliveira <otavio.pedro@outlook.com>

---
+++ teste1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+main()
+{
+	int a=12,b=15,res=0;
+	printf("Teste .....");
+	system("pause");
+	res = a+b;
+	printf("\n %d",res);
+
+}
